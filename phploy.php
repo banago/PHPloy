fbd7f760c50ee196d0dea92e55a5a4e664c5785c
Baki Goxhaj <banago@gmail.com>
2014-08-09 15:17:03 +0200

    Several further improvements and a ton of testing.
    Signed-off-by: Baki Goxhaj <banago@gmail.com>

--- phploy.php
+++ phploy.php
@@ -1,19 +1,5 @@
 <?php
-/**
- * Configuration
- */
-ini_set('memory_limit', '512M');
-set_time_limit(60*60*60); // 60 min
-define('BASE_PATH', __DIR__);
-
-/**
- * Base
- */
-ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . BASE_PATH);
-set_include_path(get_include_path() . PATH_SEPARATOR . BASE_PATH);
-
-
-require __DIR__.'/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 use Banago\PHPloy\PHPloy;
 use Banago\PHPloy\Ansi;
