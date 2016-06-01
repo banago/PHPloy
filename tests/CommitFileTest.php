1b50dbc5a54b03176cd32e524c50dd1a9ad48df8
Martin Brugger <martin@brugger.eu>
2016-06-01 21:01:49 +0100

    Added delete file test and refactored tests to be better readable

--- tests/CommitFileTest.php
+++ tests/CommitFileTest.php
@@ -6,43 +6,72 @@ class CommitFileTest extends PHPloyTestCase
 {
   protected $git;
 
+  public function testSyncAddedFileShouldSucceed()
+  {
+    $this->givenRepositoryWithConfiguration();
+    $this->whenFileIsAdded();
+    $this->thenRepositoryIsSynchronizedSuccessfully();
+  }
+
+  public function testSyncDeletedFileShouldSucceed()
+  {
+    $this->givenSynchronizedRepositoryWithSingleFile();
+    $this->whenFileIsDeleted();
+    $this->thenRepositoryIsSynchronizedSuccessfully();
+  }
+
+  public function testSyncChangedFileShouldSucceed()
+  {
+    $this->givenSynchronizedRepositoryWithSingleFile();
+    $this->whenFileIsChanged();
+    $this->thenRepositoryIsSynchronizedSuccessfully();
+  }
+
+  // test helper methods
   protected function givenRepositoryWithConfiguration()
   {
     $this->git = Repository::open($this->repository, '/usr/bin/git', 0755);
-    $commit = $this->git->writeFile('test.txt', 'Test', 'Added test.txt');
     $result = $this->git->transactional(function(TQ\Vcs\Repository\Transaction $t) {
       copy(realpath(dirname(__FILE__)."/resources/phploy.ini"), $this->repository."/phploy.ini");
       $t->setCommitMsg('Add configuration');
     });
   }
 
+  protected function givenSynchronizedRepositoryWithSingleFile()
+  {
+    $this->givenRepositoryWithConfiguration();
+    $this->whenFileIsAdded();
+    $this->thenRepositoryIsSynchronizedSuccessfully();
+  }
+
+  protected function whenFileIsAdded()
+  {
+    $commit = $this->git->writeFile('test.txt', 'Test', 'Added test.txt');
+    $this->whenRepositoryIsSynchronized();
+  }
+
   protected function whenFileIsDeleted()
   {
     $result = $this->git->transactional(function(TQ\Vcs\Repository\Transaction $t) {
       unlink($this->repository."/test.txt");
-      $t->setCommitMsg('Add configuration');
+      $t->setCommitMsg('Deleted test.txt');
     });
+    $this->whenRepositoryIsSynchronized();
   }
 
-  public function testSyncCommittedFileShouldSucceed()
+  protected function whenFileIsChanged()
   {
-    $this->givenRepositoryWithConfiguration();
+    $result = $this->git->transactional(function(TQ\Vcs\Repository\Transaction $t) {
+      $myfile = file_put_contents($this->repository."/test.txt", "change".PHP_EOL , FILE_APPEND);
+      $t->setCommitMsg('Changed test.txt');
+    });
     $this->whenRepositoryIsSynchronized();
-    $this->assertEquals(0, $this->synchronizationResult);
-    $this->assertShareInSync();
   }
 
-  public function testSuncDeletedFileShouldSucceed()
+  protected function thenRepositoryIsSynchronizedSuccessfully()
   {
-    $this->givenRepositoryWithConfiguration();
-    $this->whenRepositoryIsSynchronized();
     $this->assertEquals(0, $this->synchronizationResult);
     $this->assertShareInSync();
-
-    $this->whenFileIsDeleted();
-    $this->whenRepositoryIsSynchronized();
-    $this->assertEquals(0, $this->synchronizationResult);
-    $this->assertShareInSync();
-
   }
+
 }
