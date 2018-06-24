24440316d0811cc7811234f2cf2a0083e9752236
Bastien <bastien.boussouf@penbase.com>
2018-06-25 01:37:00 +0200

    Pull both images

--- tests/docker_image_setup.sh
+++ tests/docker_image_setup.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 docker pull atmoz/sftp
-if [ $? != 0 ]; then
-    tmp="/tmp/atmoz-sftp"
-    mkdir -p ${tmp}
-    wget -O "${tmp}/master.zip" https://github.com/atmoz/sftp/archive/master.zip
-    (cd "${tmp}" && unzip master.zip)
-    (cd "${tmp}" && docker build .)
-fi
+docker pull stilliard/pure-ftpd:hardened
